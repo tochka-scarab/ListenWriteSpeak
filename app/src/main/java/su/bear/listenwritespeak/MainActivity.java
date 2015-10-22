16da8396d67d29e711ec2a9690600fe8035fdb5e
tochka-scarab <tochka.scarab@gmail.com>
2015-10-22 15:37:56 +0300

    Инициализация и запуск голосового движка

--- app/src/main/java/su/bear/listenwritespeak/MainActivity.java
+++ app/src/main/java/su/bear/listenwritespeak/MainActivity.java
@@ -14,13 +14,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -74,6 +77,32 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
+        //засекаем щелчок пользователя по слову из списка
+        wordList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            //метод вызывается в ответ на щелчок по слову
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                //записываем в переменную TextView строки
+                TextView wordView = (TextView) view;
+                //получаем строку с текстом
+                String wordChosen = (String) wordView.getText();
+                //выводим Toast сообщение
+                Toast.makeText(MainActivity.this, R.string.you_said + wordChosen, Toast.LENGTH_SHORT).show();
+
+                repeatTTS.speak(R.string.you_said + wordChosen, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        //подготовка движка TTS для проговаривания слов
+        Intent checkTTSIntent = new Intent();
+        //проверка наличия TTS
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        //запуск checkTTSIntent интента
+        startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
+
     }
 
     public void onClick(View v)
@@ -102,20 +131,49 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    protected void onActivityResult(int requestCod, int resultCode, Intent data)
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         //проверяем результат распознавания речи
-        if (requestCod == VR_REQUEST&&resultCode == RESULT_OK)
+        if (requestCode == VR_REQUEST&&resultCode == RESULT_OK)
         {
             //Добавляем распознанные слова в список результатов
             ArrayList<String> suggestedWords = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
             //Передаем список возможных слов через ArrayAdapter компоненту ListView
-            wordList.setAdapter (new ArrayAdapter<String>(this, R.layout.word, suggestedWords));
+            wordList.setAdapter (new ArrayAdapter<>(this, R.layout.word, suggestedWords));
         }
-        //tss код здесь
-
+        //tss код здесь returned from TTS data check
+        if (requestCode == MY_DATA_CHECK_CODE)
+        {
+            //все необходимые приложения установлены, создаем TTS
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS)
+            {
+                repeatTTS = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener()
+                {
+                    @Override
+                    public void onInit(int status)
+                    {
+                    }
+                });
+            }
+            else
+            {
+                //движок не установлен, предположим пользователю установить его
+                //интент, перебрасывающий пользователя на страницу TSS в Google Play
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
         //вызываем метод родительского класса
-        super.onActivityResult(requestCod, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void onInit(int initStatus)
+    {
+        if (initStatus == TextToSpeech.SUCCESS)
+        {
+            repeatTTS.setLanguage(Locale.UK);//Язык
+        }
     }
 
     @Override
